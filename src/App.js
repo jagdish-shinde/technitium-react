dacc040be36b6546dfd10b7b8ee46280325b4097
jaGdish <jagdish.shinde@functionup.org>
2023-05-02 15:26:14 +0530

    session : fetch methode and async await

--- src/App.js
+++ src/App.js
@@ -1,16 +1,83 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-import List from './components/molecules/list/list';
-import { number , names } from './components/const';
-import RegistrationForm from './components/organisms/form/form';
+
 
 function App() {
 
+  const [image , setImage] = useState('https://images.pexels.com/photos/3177388/pexels-photo-3177388.jpeg?auto=compress&cs=tinysrgb&w=800')
+
+
+  //  https://jsonplaceholder.typicode.com/posts
+
+  // https://images.dog.ceo/breeds/entlebucher/n02108000_352.jpg
+  //  fetch methode -/> used to request the server  , browser
+  //  
+
+  
+    // https://jsonplaceholder.typicode.com/posts
+    // https://restcountries.com/v2/all
+    // https://api.chucknorris.io/jokes/random
+    // https://dog.ceo/api/breeds/image/random
+
+
+  async function getDogData () {
+    // alert(3543)
+
+    //  we wil hit api
+    //  it gives data {mesage : 'new-dog-image-link'
+    // success -/> then -/> function 
+    // failure -/> catch
+    // console.log({data})
+
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then((data)=> data.json())
+    .then ((response)=> setImage(response.message))
+    .catch((error) => console.log(error))  
+
+  }
+
+  async function getDog () {
+
+    try{
+        // async await (to wait till request is processed
+        const response = await fetch('https://dog.ceo/api/breeds/image/random')
+        const data = await response.json()
+        setImage(data.message)
+    }
+    catch(error){
+      console.log('i am from catch' ,error)
+    }
+
+
+  }
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
   return (
     <div className="App">
-      {/* <List array = {number}/>
-      <List array = {names}/> */}
-      <RegistrationForm/>
+      <img 
+        src={image}
+        height={'200px'}
+        width={'200px'}
+        alt='dog image'
+      />
+
+      <button 
+      // onClick={getDogData}
+      onClick = {getDog}
+      >Get Dog</button>
+
     </div>
   );
 }

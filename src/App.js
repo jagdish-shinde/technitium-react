e0158c8b7a6fa3438190560c4603767f0b6e9fac
jaGdish <jagdish.shinde@functionup.org>
2023-05-22 14:42:35 +0530

    solution : quiz

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import QuizApp from './components/organisms/quiz-view/quiz-view';
+import { useState } from 'react';
 
 function App() {
+  const questions = [
+    {
+      question : 'what is 1+1 ?' ,
+      options : [ 2,3,4,5] ,
+      correctAns : 2 ,
+      id :1
+    },
+    {
+      question : 'what is 2+2 ?' ,
+      options : [ 2,3,4,5] ,
+      correctAns : 4 ,
+      id : 2
+    },
+    {
+      question : 'what is 3+3 ?' ,
+      options : [ 2,3,4,5] ,
+      correctAns : 2 ,
+      id :3
+    }
+  ]
+  const [currectQuestionIndex , setCurrentQuestionIndex] = useState(0)
+  const [finalScore , setFinalScore] = useState(0)
+
+  function changeQuestion () {
+    setCurrentQuestionIndex(currectQuestionIndex + 1)
+  }
+  function handleSubmit () {
+    // submit ka code write here
+    alert('submitto the quiz')
+    // just read the final score value
+    setFinalScore(
+      //  here to save final score read from local storage
+    )
+  }
+  const isLastQuestion = currectQuestionIndex + 1 === questions.length
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QuizApp 
+        questionData = {questions[currectQuestionIndex]}
+        changeQuestion = {changeQuestion}
+        isLastQuestion = {isLastQuestion}
+        handleSubmit = {handleSubmit}
+      />
+    
     </div>
   );
 }
---
+++ src/components/organisms/quiz-view/quiz-view.js
@@ -0,0 +1,48 @@
+import styles from './quiz-view.module.css'
+
+export default function QuizApp ({questionData,changeQuestion,isLastQuestion,handleSubmit}) {
+
+
+    const {
+        question ,
+        options ,
+        correctAns ,
+        id ,
+    } = questionData || {}
+
+    function saveQuizResponse ( selectedOption , questionId ) {
+        // console.log({selectedOption , questionId})
+        const isAnswerCorrect = selectedOption === correctAns
+        const score = isAnswerCorrect ? 2 : 0
+
+        localStorage.setItem('finalScore' ,score )
+        
+
+    }
+
+    return (
+        <div className={styles.container}>
+            <p>{question}</p>
+            <div className={styles.options}>
+                {
+                    options.map(option => (
+                        <div key={option}>
+                            <input 
+                                type='radio' 
+                                name='quizOptions' 
+                                onChange={()=> saveQuizResponse(option,id)}
+                            />
+                            <span>{option}</span>
+                        </div>
+                    ))
+                }
+
+            </div>
+            <div className={styles.btnContainer}>
+                <button onClick={ isLastQuestion ? handleSubmit :  changeQuestion}>{
+                    isLastQuestion ? 'Submit' : 'Next'
+                }</button>
+            </div>
+        </div>
+    )
+}
